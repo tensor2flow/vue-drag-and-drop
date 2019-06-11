f57c3f6df6e1f6933a5d2e90776b7062356fb988
Beknazarov <tensor2flow@gmail.com>
2019-06-11 15:54:08 +0500

    read me

--- mixin/mounted.js
+++ mixin/mounted.js
@@ -18,31 +18,30 @@ function delegateAndEmit(evtName) {
 }
 
 const mounted = function() {
-  this.noneFunctionalComponentMode =
-    this.getTag().toLowerCase() !== this.$el.nodeName.toLowerCase();
+  this.noneFunctionalComponentMode = this.getTag().toLowerCase() !== this.$el.nodeName.toLowerCase()
   if (this.noneFunctionalComponentMode && this.transitionMode) {
-    throw new Error(`Transition-group inside component is not supported. Please alter tag value or remove transition-group. Current tag value: ${this.getTag()}`);
+    throw new Error(`Transition-group inside component is not supported. Please alter tag value or remove transition-group. Current tag value: ${this.getTag()}`)
   }
-  var optionsAdded = {};
+  var optionsAdded = {}
   eventsListened.forEach(elt => {
     optionsAdded['on' + elt] = delegateAndEmit.call(this, elt);
-  });
+  })
 
   eventsToEmit.forEach(elt => {
     optionsAdded['on' + elt] = emit.bind(this, elt)
-  });
+  })
 
   const attributes = Object.keys(this.$attrs).reduce((res, key) => {
     res[camelize(key)] = this.$attrs[key]
     return res
-  }, {});
+  }, {})
 
   const options = Object.assign({}, this.options, attributes, optionsAdded, {
     onMove: (evt, originalEvent) => {
       return this.onDragMove(evt, originalEvent)
     }
-  });
-  !('draggable' in options) && (options.draggable = '>*')
+  })
+	!('draggable' in options) && (options.draggable = '>*')
   this._sortable = new Sortable(this.rootContainer, options)
   this.computeIndexes()
 }
